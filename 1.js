f8ffbf4a939990125700b35499d7380ee29abb30
= <=>
2020-01-31 22:28:46 +0700

    co ban xong file 1.js

---
+++ 1.js
@@ -0,0 +1,23 @@
+﻿ $(function(){
+ 	//viet cho nut
+ 	$("nav ul li a").click(function(event) {
+ 		// phat hien data class cua nut duoc click
+ 		var danhmuc = $(this).data("class");
+ 		var ten = $(this).text();// $(this).text() la lay noi dung text ben trong mot the
+
+ 		$("section h1").text(ten);//text(ten) la dat ten,abc,..., vao trong mot the nao do
+
+ 		$(".content ul li").each(function() {
+ 			if($(this).hasClass(danhmuc))
+ 			{
+ 				$(this).show(300);
+ 			}
+ 			else
+ 			{
+ 				$(this).hide(300);
+ 			}
+ 		});
+ 		return false;
+ 	});
+})  
+ 
